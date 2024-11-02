5a20fd2366cae6ca2f4b61f421189b2cb00bdd19
Ando Keiko <gajumaro.no.ki@gmail.com>
2024-11-02 20:45:09 +0900

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+
+  public static void main(String[] args) {
+    //文字列結合と数値計算結果の結合をして
+    //自分の名前+自分の年齢+"歳です。"が出力されるようにする。
+    String name = "安藤佳子";
+    int age = 29;
+    System.out.println(name + age + "歳です。");
+  }
+}
